77d793f334b5e022724206d1fe8460f9a4c05256
lhywell <lihuiyin128@163.com>
2019-02-20 10:35:07 +0800

    更改webpack配置，把iview样式包打入组件库，并重新打包

--- build/webpack.tdseed.conf.js
+++ build/webpack.tdseed.conf.js
@@ -5,12 +5,21 @@ const webpackBaseConfig = require('./webpack.tdseed.default.js');
 const CompressionPlugin = require('compression-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const utils = require('./utils')
 
 process.env.NODE_ENV = 'production';
 
 module.exports = merge(webpackBaseConfig, {
     devtool: 'source-map',
+    module: {
+        rules: utils.styleLoaders({
+            sourceMap: true,
+            extract: true,
+            usePostCSS: true
+        })
+    },
     entry: {
         main: './src/index.js'
     },
@@ -36,12 +45,24 @@ module.exports = merge(webpackBaseConfig, {
             'process.env.NODE_ENV': '"production"'
         }),
         new ExtractTextPlugin({
-            filename: utils.assetsPath('[name].css'),
+            filename: utils.assetsPath('tdseed.css'),
             // Setting the following option to `false` will not extract CSS from codesplit chunks.
             // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
             // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
             // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
             allChunks: true,
+        }),
+        new OptimizeCSSPlugin({
+            cssProcessorOptions: { safe: true, discardComments: { removeAll: true } }
+        }),
+        new webpack.HashedModuleIdsPlugin(),
+        // enable scope hoisting
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'app',
+            async: 'vendor-async',
+            children: true,
+            minChunks: 3
         }),
         new UglifyJsPlugin({
             parallel: true,
