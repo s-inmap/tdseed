4219ac4071d2a12c6ead0b0a2daea4542e20a5a3
lhywell <lihuiyin128@163.com>
2019-02-21 11:42:30 +0800

    组件filter-city更改

--- example/router/filter-city.vue
+++ example/router/filter-city.vue
@@ -1,66 +1,31 @@
 <template>
     <div id="ex-filter-city">
-        <div class="position">
-            <Poptip placement="bottom-end" width="220" v-model="visible" @on-popper-show="onPopperShow">
-                <div class="cityPop hoverPop">
-                    <i class="icon-funnel"></i>选择城市
-                </div>
-                <div class="api" slot="content">
-                    <filter-city v-model="checkCity" :data="selectCityData" @on-change="cityChange" @on-submit="submitCity" @on-cancel="cancelCity"></filter-city>
-                </div>
-            </Poptip>
-        </div>
+        <filter-city :data="pageList" @on-change="onChange" @on-submit="onSubmit" @on-cancel="onCancel" @on-popper-show="onPopperShow"></filter-city>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            visible: false,
-            checkCity: [],
-            selectCityData: [],
             pageList: [{ "serviceType": 3, "score": 44.0, "lng": "116.643422281780261", "polygon": null, "serviceArea": 125600.0, "city": "北京市", "brandId": 240, "name": "麦当劳(迎宾中路餐厅)", "serviceScope": 200, "id": 2290, "lat": "40.321470735564624", "status": "SUCCESS" }]
         }
     },
     methods: {
-        onPopperShow() {
-            this.selectCityData.push(this.pageList);
+        onPopperShow() {},
+        onChange(value) {
+            console.log(value)
         },
-        cityChange(value) {
-            this.checkCity = value;
+        onSubmit(value) {
+            console.log(value)
         },
-        submitCity(value) {
-            this.visible = false;
-        },
-        cancelCity(value) {
-            this.visible = false;
+        onCancel(value) {
+            console.log(value)
         }
     }
 }
 
 </script>
 <style>
-#ex-filter-city {
-    position: relative;
-    .position {
-        position: absolute;
-        top: 0;
-        left: 0;
-        .cityPop {
-            line-height: 32px;
-            width: 104px;
-            cursor: pointer;
-            text-align: center;
-            font-size: 14px;
-            color: var(--color-hover);
-        }
-        .ivu-poptip-body {
-            padding: 0;
-        }
-        .ivu-poptip-body-content {
-            overflow: hidden;
-        }
-    }
-}
+#ex-filter-city {}
 
 </style>
