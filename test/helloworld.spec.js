eb5cdff8b099079483e171eabe03e553924ccf2a
huiyin.li <huiyin.li@tendcloud.com>
2019-05-31 10:13:06 +0800

    增加jest单元测试

---
+++ test/helloworld.spec.js
@@ -0,0 +1,8 @@
+function helloWorld() {
+    return 'Hello World'
+}
+describe('hello world', () => {
+    it('should render correct result', () => {
+        expect(helloWorld()).toEqual('Hello World')
+    })
+})
